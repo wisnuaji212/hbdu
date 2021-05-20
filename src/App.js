e4b6d3524c0de8a86092a0d417cbf8746acd33c5
wisnuaji212 <59964421+wisnuaji212@users.noreply.github.com>
2021-05-20 17:34:55 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ class App extends Component {
       nama: [],
       confetti: true,
       count_click: 0,
-      time: 60,
+      time: 20,
     };
     this.url = require("./asset/music2.mp3");
     this.audio = new Audio(this.url);
@@ -96,7 +96,7 @@ class App extends Component {
   };
 
   hitungmundur = () => {
-    var waktu = 60;
+    var waktu = 20;
     setInterval(() => {
       waktu--;
       if (waktu < 0) {
